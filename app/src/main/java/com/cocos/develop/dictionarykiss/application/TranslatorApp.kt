def4c1739aa1834ed419d83c6b6bdabcae6e6075
Amina <amina1000@yandex.ru>
2021-09-17 19:47:55 +0300

    Lesson 6
    Практическое задание:
    
    1. Добавьте в свой проект динамическую фичу

--- app/src/main/java/com/cocos/develop/dictionarykiss/application/TranslatorApp.kt
+++ app/src/main/java/com/cocos/develop/dictionarykiss/application/TranslatorApp.kt
@@ -1,9 +1,6 @@
 package com.cocos.develop.dictionarykiss.application
 
 import android.app.Application
-import com.cocos.develop.dictionarykiss.di.application
-import com.cocos.develop.dictionarykiss.di.favoriteScreen
-import com.cocos.develop.dictionarykiss.di.mainScreen
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,8 +9,7 @@ class TranslatorApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidContext(applicationContext)
-            modules(listOf(application, mainScreen, favoriteScreen))
+            androidContext(this@TranslatorApp)
         }
     }
 }
